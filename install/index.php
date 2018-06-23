82ea516da6e096dac95356d738c6c837a7797fbb
Dany <7019732@qq.com>
2018-06-23 12:22:01 +0800

    Ver_1.0.3

--- install/index.php
+++ install/index.php
@@ -324,7 +324,9 @@ if($e==0) {
 		<label for="name">当前网址域名(一般默认):</label>
 		<input type="text" class="form-control" id="domain" value="{$url}">
 		<label for="name">拇指付用户ID:</label>
-		<input type="text" class="form-control" id="id" value="{$json['id']}">
+		<input type="text" class="form-control" id="appid" value="{$json['id']}">
+		<label for="name">拇指付APPID:</label>
+		<input type="text" class="form-control" id="id" value="{$json['appid']}">
 		<label for="name">拇指付access_token:</label>
 		<input type="text" class="form-control" id="access_token" value="{$json['access_token']}">
 		<br><input type="submit" class="btn btn-primary btn-block" onclick="connect()" value="远程连接"><br/>
@@ -423,7 +425,8 @@ if(file_put_contents('../includes/mzf_config.php',$configx)){
 	<div class="panel-body">
 <?php
 	@file_put_contents("install.lock",'安装锁');
-	echo '<div class="alert alert-success"><font color="green">安装完成！管理账号和密码是:admin/admin</font><br/><br/><a href="../">>>网站首页</a>｜<a href="../admin/">>>后台管理</a><hr/>更多设置选项请登录后台管理进行修改。<br/><br/><font color="#FF0033">如果你的空间不支持本地文件读写，请自行在install/ 目录建立 install.lock 文件！</font></div>';
+	unlink("../includes/json.txt");
+	echo '<div class="alert alert-success"><font color="green">安装完成！管理账号和密码是:admin/admin</font><br/><br/><a href="../">>>网站首页</a>｜<a href="../admin/">>>后台管理</a><hr/>更多设置选项请登录后台管理进行修改。<br/><br/><font color="#FF0033">如果你的空间不支持本地文件读写，请自行在install/ 目录建立 install.lock 文件和删除includes/json.txt文件！</font></div>';
 }
 ?>
 	</div>
@@ -440,15 +443,16 @@ $(document).keyup(function(event){
 function connect(){
    $("#parsley-id").text("");
    var domain = $("#domain").val(), 
-       id = $("#id").val(), 
-	   access_token = $("#access_token").val();
+       id = $("#id").val(),
+       appid = $("#appid").val(),
+       access_token = $("#access_token").val();
    layer.msg('正在连接···', {icon: 16,shade: 0.01,time: 15000}); 
    $.ajax( {
 	  type: "POST",
 	  url: "https://api.muzhifu.cc/Yizf/get.php", 
 	  dataType : 'json',
 	  timeout : 15000, //超时时间设置，单位毫秒
-	  data: "domain="+domain+"&id="+id+"&access_token="+access_token,
+	  data: "domain="+domain+"&appid="+appid+"&id="+id+"&access_token="+access_token,
 	  success: function(data){
 		  if(data.code == 0){
 			  $("#parsley-id").html(data.msg);
