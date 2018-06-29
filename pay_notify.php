8d213a7999db195650b7b1a80f17e03c03fbd3fe
Dany <7019732@qq.com>
2018-06-29 20:32:32 +0800

    Ver_1.0.4

--- pay_notify.php
+++ pay_notify.php
@@ -5,22 +5,22 @@
  * 异步通知文件
  */
 require_once("./includes/common.php");
-require_once("./includes/muzhifu/muzhifu.config.php");
+require_once("./includes/muzhifu/muzhifu_config.php");
 require_once("./includes/muzhifu/lib/muzhifu_submit.php");
-$sign = Muzhifu::sign($_POST, $muzhifu_config['key']);
-if ($sign == $_POST['sign']) {
-	if ($app_id == $_POST['app_id']) {
-		$out_trade_no = $_POST['out_trade_no'];
-		$trade_no = $_POST['trade_no'];
-		$trade_status = $_POST['trade_status'];
+$sign = Muzhifu::sign($_GET, $muzhifu_config['key']);
+if ($sign == $_GET['sign']) {
+	if ($muzhifu_config['partner'] == $_POST['pid']) {
+		$out_trade_no = $_GET['out_trade_no'];
+		$trade_no = $_GET['trade_no'];
+		$trade_status = $_GET['trade_status'];
 		$srow = $DB->query("SELECT * FROM pay_order WHERE trade_no='{$out_trade_no}' limit 1")->fetch();
-		if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
+		if ($_GET['trade_status'] == 'TRADE_SUCCESS') {
 			$DB->query("update `pay_order` set `status` ='1',`endtime` ='$date',`buyer` ='$buyer_email' where `trade_no`='$out_trade_no'");
 			$addmoney=round($srow['money']*$conf['money_rate']/100,2);
 			$DB->query("update pay_user set money=money+{$addmoney} where id='{$srow['pid']}'");
 			$url=creat_callback($srow);
 			curl_get($url['notify']);
-			proxy_get($url['notify']);
+			//proxy_get($url['notify']);
 		}
 		echo 'success';
 	}
--- pay_return.php
+++ pay_return.php
@@ -18,7 +18,7 @@ require_once("./includes/muzhifu/lib/muzhifu_submit.php");
 <?php
 $sign = Muzhifu::sign($_GET, $muzhifu_config['key']);
 if ($sign == $_GET['sign']) {
-    if ($app_id == $_GET['app_id']) {
+    if ($muzhifu_config['partner'] == $_GET['pid']) {
         $out_trade_no = $_GET['out_trade_no'];
         $trade_no = $_GET['trade_no'];
         $trade_status = $_GET['trade_status'];
@@ -29,7 +29,7 @@ if ($sign == $_GET['sign']) {
             $DB->query("update pay_user set money=money+{$addmoney} where id='{$srow['pid']}'");
             $url = creat_callback($srow);
             curl_get($url['notify']);
-            proxy_get($url['notify']);
+            //proxy_get($url['notify']);
         }
         echo 'success';
     }
@@ -51,7 +51,7 @@ if ($sign == $_GET['sign']) {
         sysmsg('未成功支付！' . "out_trade_no=" . $out_trade_no);
     }
 } else {
-    sysmsg('验证失败' . "out_trade_no=" . $srow['out_trade_no']);
+    sysmsg('验证失败' . "out_trade_no=" . $out_trade_no);
 }
 ?>
 </body>
